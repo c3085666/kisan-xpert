72b75f6125afea6adee9ca41fafb49410d58a70e
c3085666 <c3085666@hallam.shu.ac.uk>
2025-01-16 23:38:11 +0000

    Initial Commit

--- New Folder/src/app/pages/shared/shared.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { DatetimeDifferencePipe } from '../../pipe/datetime-difference.pipe';
-@NgModule({
-  declarations: [DatetimeDifferencePipe],
-  exports: [DatetimeDifferencePipe]
-})
-export class SharedModule { }
--- New Folder/src/app/pipe/datetime-difference.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DatetimeDifferencePipe } from './datetime-difference.pipe';
-
-describe('DatetimeDifferencePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DatetimeDifferencePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
